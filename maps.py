4f991f10c9e075c74f505401d2c6fa431f51c359
1spyral <83098914+1spyral@users.noreply.github.com>
2023-10-14 05:15:15 -0400

    Update maps.py

--- maps.py
+++ maps.py
@@ -1,4 +1,16 @@
 import googlemaps
 from secret import gmap_token
 
-gmaps = googlemaps.Client(key=gmap_token)
+class maps():
+    def __init__(self):
+        self.client = googlemaps.Client(key=gmap_token)
+
+    def set_location(self, location: [float, float]) -> None:
+        self.location = location
+
+    # Generate the string for the location bias
+    def __generate_location_bias(self) -> str:
+        return "circle" + self.radius + "@" + ",".join(map(str, self.location))
+
+    def search(self, query: str): #TODO: specify output type
+        self.client.find_place(query, "textquery", [], self.__generate_location_bias(), "en")
