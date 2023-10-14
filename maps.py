e7739fc1c6eb43707ffae8f83b3e0cbf1016baea
withercraft303 <lsntao.withercraft303@gmail.com>
2023-10-14 05:39:01 -0400

    5:40 am push

--- maps.py
+++ maps.py
@@ -10,7 +10,15 @@ class maps():
 
     # Generate the string for the location bias
     def __generate_location_bias(self) -> str:
-        return "circle" + self.radius + "@" + ",".join(map(str, self.location))
+        return "circle:" + self.radius + "@" + ",".join(map(str, self.location))
 
     def search(self, query: str): #TODO: specify output type
-        self.client.find_place(query, "textquery", [], self.__generate_location_bias(), "en")
+        return self.client.find_place(query, "textquery", [], self.__generate_location_bias(), "en")
+
+    def search_by_id(self, id) :
+        return self.client.place(id)
+
+#maps = maps()
+#maps.set_location([43.90315162960289, -79.43975174603027])
+#a = maps.search("Mon Sheong Restaurant")
+#print(maps.search_by_id(a['candidates'][0]['place_id'])['result'])
---
+++ place.py
@@ -0,0 +1,11 @@
+class Place:
+    def __init__(self, _address : str, _name : str, _rating: float, time: Hours, type: list):
+        #self.address = 
+        pass
+
+    def __init__(self, _raw : dict):
+        pass
+
+class Hours:
+    def __init__(self, _raw : dict):
+        pass
