c191c09d9d41b895623f1dac4eaff467c8651886
Andrew <67875282+andrewchu16@users.noreply.github.com>
2023-10-14 16:17:47 -0400

    fixed search bar

--- frontend/src/lib/components/SearchBar.svelte
+++ frontend/src/lib/components/SearchBar.svelte
@@ -3,12 +3,12 @@
 </script>
 
 <div
-	class="drop-shadow-lg w-[332px] bg-light h-[51px] rounded-[28px] py-3 px-5 focus-within:bg-accent text-accent focus-within:text-light flex justify-between items-center transition-colors"
+	class="drop-shadow-lg w-[332px] bg-light h-[51px] rounded-[28px] py-3 px-5 focus-within:bg-accent text-accent focus-within:text-light flex justify-between items-center transition-all"
 >
 	<input
 		type="text"
 		placeholder="Search"
-		class="font-bold focus:font-normal inline outline-none placeholder:text-accent bg-transparent focus:placeholder:text-light"
+		class="font-bold focus:font-normal outline-none placeholder:text-accent bg-transparent focus:placeholder:text-light text-[20px] w-[224px]"
 	/>
-	<Search />
+	<Search class="w-[32px] h-[32px] block"/>
 </div>
