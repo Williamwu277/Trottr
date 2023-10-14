d39b0431b3f77d34b701cee1972f0cd87850347e
1spyral <83098914+1spyral@users.noreply.github.com>
2023-10-14 05:22:14 -0400

    Update maps.py

--- maps.py
+++ maps.py
@@ -5,12 +5,12 @@ class maps():
     def __init__(self):
         self.client = googlemaps.Client(key=gmap_token)
 
-    def set_location(self, location: [float, float]) -> None:
-        self.location = location
+    def set_location(self, lat: float, lng: float) -> None:
+        self.location = (lat, lng)
 
     # Generate the string for the location bias
     def __generate_location_bias(self) -> str:
-        return "circle" + self.radius + "@" + ",".join(map(str, self.location))
+        return "circle:" + '1000' + "@" + ",".join(map(str, self.location)) #TODO: make radius customizable and its own attribute
 
     def search(self, query: str): #TODO: specify output type
         self.client.find_place(query, "textquery", [], self.__generate_location_bias(), "en")
