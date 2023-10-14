736caf6c1bce8a51c771c59845df465bf0fb354a
withercraft303 <lsntao.withercraft303@gmail.com>
2023-10-14 10:29:38 -0400

    +id to place

--- place.py
+++ place.py
@@ -29,19 +29,22 @@ class Period:
 
 
 class Place:
-    def __init__(self, address: str = None, name: str = None, rating: float = None, 
+    def __init__(self, place_id: str, address: str = None, name: str = None, rating: float = None, 
                     time: OpeningHours = None, price_rating: int = None, categories: list = []):
+        self.place_id = place_id
         self.address = address
         self.name = name
         self.rating = rating
         self.open_hours = time
         self.price_rating = price_rating
         self.categories = categories
+
         
         
 
     def from_raw(self, raw: dict) -> None:
         #self.address = raw[""]
+        self.place_id = raw['place_id']
         self.name = raw["name"]
         self.open_hours = OpeningHours(raw["current_opening_hours"])
         self.price_rating = int(raw["price_level"])
