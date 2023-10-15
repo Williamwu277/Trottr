136a120290979e70cf7d3cebf93511e3930bb4bf
Andrew <67875282+andrewchu16@users.noreply.github.com>
2023-10-15 06:57:36 -0400

    refactors to point

--- frontend/src/lib/components/PoiCard.svelte
+++ frontend/src/lib/components/PoiCard.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import DragHandle from '~icons/material-symbols/drag-handle-rounded';
-
 	export let name: string;
 	export let time: string;
 </script>
@@ -11,6 +9,5 @@
 			<h2 class="text-[20px] font-bold text-ellipsis">{name}</h2>
 			<h3 class="text-[20px]">{time}</h3>
 		</div>
-		<DragHandle class="w-[18px] h-[12px]" />
 	</div>
 </div>
--- frontend/src/routes/list/+page.server.ts
+++ frontend/src/routes/list/+page.server.ts
@@ -1,14 +1,15 @@
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
-	const res = await fetch('localhost:5000/distance', {
-		method: 'GET',
-	});
+	// const res = await fetch('localhost:5000/distance', {
+	// 	method: 'GET',
+	// });
 
-	const json = await res.json();
-	const loc = JSON.parse(json);
+	// const json = await res.json();
+	// const loc = JSON.parse(json);
 
 	return {
-		locations: loc
+		// locations: loc
+		locations: []
 	};
 };
